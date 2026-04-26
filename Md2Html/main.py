76b5928e21118ee5931f7cb3cd61b81ff843008d
SuhArtem <sukhodolskiyartem@gmail.com>
2026-04-26 14:42:32 +0500

    Delete Md2Html directory

--- Md2Html/main.py
+++
@@ -1,5 +0,0 @@
-from handlers.Md2Hhtml import Md2Html
-
-mh = Md2Html("input.txt", "output.txt")
-mh.toHtml()
-
--- Md2Html/markup/text.py
+++
@@ -1,9 +0,0 @@
-from markup.markup import MarkUp
-
-class Text(MarkUp):
-
-    def __init__(self, string: str):
-        self.val = string
-
-    def toHtml(self):
-        return self.val
