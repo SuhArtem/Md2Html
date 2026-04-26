5bad1fcf8a7e1e0a945a708382be3a932dc9954c
SuhArtem <sukhodolskiyartem@gmail.com>
2026-04-26 14:41:33 +0500

    console update
    Now the program can be start from terminal with two arguments: input.txt file and output.txt file

--- main.py
+++ main.py
@@ -1,5 +1,24 @@
+import sys
 from Md2Hhtml import Md2Html
 
-mh = Md2Html("input.txt", "output.txt")
-mh.toHtml()
+def isTxt(filename: str):
+    return filename.endswith(".txt")
 
+def main():
+    args = sys.argv[1:]
+    try:
+        inputFile = args[0]
+        outputFile = args[1]
+
+        if not isTxt(inputFile) or not isTxt(outputFile):
+            print("Files must have .txt extension")
+            sys.exit()
+
+        mh = Md2Html(args[0], args[1])
+        mh.toHtml()
+    except IndexError:
+        print("It is necessary to specify two files in the arguments: input and output")
+        sys.exit()
+
+if __name__ == "__main__":
+    main()
